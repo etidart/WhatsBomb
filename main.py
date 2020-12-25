aafdea874badd709e947ce53f3054d64ba4657b2
Arseniy <53515110+etidart-root@users.noreply.github.com>
2020-12-25 15:11:23 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import eel, pyautogui
+
+eel.init("web")
+
+
+@eel.expose
+def bomb(t, txt):
+    times = 0
+    while times < t:
+        pyautogui.typewrite(txt)
+        pyautogui.press("enter")
+        times = times + 1
+
+
+eel.start("main.html", size=(700, 740))
